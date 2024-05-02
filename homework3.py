20cfddff0cb809715cfc8ab2324257f559bda763
UmarL9107 <168477540+UmarL9107@users.noreply.github.com>
2024-05-02 22:28:07 +0400

    Add files via upload
    my_string = input('Good Morning, Umar ! ')
    print(len(my_string))
    
    my_string = 'Good Morning, Umar ! '.upper()
    print(my_string)
    
    my_string = 'Good Morning, Umar ! '.lower()
    print(my_string)
    
    print('Good Morning, Umar ! '.replace(' ',''))
    
    print(my_string[0])
    print(my_string[-1])

---
+++ homework3.py
@@ -0,0 +1,13 @@
+my_string = input('Good Morning, Umar ! ')
+print(len(my_string))
+
+my_string = 'Good Morning, Umar ! '.upper()
+print(my_string)
+
+my_string = 'Good Morning, Umar ! '.lower()
+print(my_string)
+
+print('Good Morning, Umar ! '.replace(' ',''))
+
+print(my_string[0])
+print(my_string[-1])
